875b57a7aa0522350a48da7b482333b7acbd44de
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-08-08 01:55:48 +0600

    view details page adds show

--- src/components/LatestNewsDetails/LatestNewsDetails.jsx
+++ src/components/LatestNewsDetails/LatestNewsDetails.jsx
@@ -4,14 +4,15 @@ import { IoCopyOutline } from "react-icons/io5";
 import { MdWhatsapp } from "react-icons/md";
 import { TiSocialFacebook } from "react-icons/ti";
 import { MdOutlineLocalPrintshop } from "react-icons/md";
+import './LatestNewsDetails.css'
 
 const LatestNewsDetails = () => {
   const adds = "https://i.ibb.co/bPctdNy/11725643505918156321.png";
   const adds1 = "https://i.ibb.co/mBQTmJR/img1.png";
   const icon = "https://i.ibb.co/QXFd0p7/icon.webp";
   const nogodAdd = "https://i.ibb.co/L8dCVjP/17716574972985266077.gif";
-  const ksrm = 'https://i.ibb.co/X8BkG9B/11762138729451261380.png'
-  const jomunaAC= 'https://i.ibb.co/NCNSSYQ/img2.png';
+  const ksrm = "https://i.ibb.co/X8BkG9B/11762138729451261380.png";
+  const jomunaAC = "https://i.ibb.co/NCNSSYQ/img2.png";
 
   const details = useLoaderData();
   const { id } = useParams();
@@ -23,10 +24,12 @@ const LatestNewsDetails = () => {
       <div className="grid lg:grid-cols-12 gap-3 py-3">
         <div className="lg:col-span-8 w-full lg:w-auto border-r-[3px] p-3">
           <h3 className="border-b-2 w-max text-black border-black">জাতীয়</h3>
-          <h2 className="text-base lg:text-2xl text-black font-medium my-3">{news_title}</h2>
+          <h2 className="text-base lg:text-2xl text-black font-medium my-3">
+            {news_title}
+          </h2>
           <hr className="bg-black border-black opacity-80 " />
 
-          <div className="my-3 flex flex-col gap-5 lg:gap-0 lg:flex-row lg:justify-between lg:items-center">
+          <div className="my-3 flex flex-col gap-5 lg:gap-0 lg:flex-row md:flex-row lg:justify-between md:justify-between lg:items-center">
             {/* profile div */}
             <div className="flex gap-3">
               <img className="w-[50px]" src={icon} alt="" />
@@ -39,7 +42,7 @@ const LatestNewsDetails = () => {
             </div>
 
             {/* icon div */}
-            <div className="flex gap-3 items-center flex-wrap-reverse">
+            <div className="flex gap-3 items-center  flex-wrap-reverse">
               <div className="text-[14px] opacity-80 ">
                 <p>957</p>
                 <p>views</p>
@@ -68,7 +71,7 @@ const LatestNewsDetails = () => {
                 <p className="">অ-</p>
               </div>
             </div>
-        </div>
+          </div>
 
           <div className="my-3">
             <img className="basis-full w-full" src={img} alt="" />
@@ -87,13 +90,13 @@ const LatestNewsDetails = () => {
             <hr className=" border-black opacity-80" />
 
             {/* table */}
-            
+
             <div className="overflow-x-auto rounded-lg border border-gray-200 my-10">
               <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
                 <thead className="">
                   <tr className="">
                     <th className=" px-4 py-2 font-bold bg-[#C2DAF5] text-black text-start">
-                    আরও পড়ুন
+                      আরও পড়ুন
                     </th>
                   </tr>
                 </thead>
@@ -101,20 +104,20 @@ const LatestNewsDetails = () => {
                 <tbody className="divide-y divide-gray-200">
                   <tr>
                     <td className="w px-4 py-2 font-medium text-gray-900 bg-[#EEF6FF] hover:text-blue-500">
-                    আইজিপির পদ থেকে সরিয়ে দেওয়া হলো চৌধুরী আব্দুল্লাহ মামুনকে
+                      আইজিপির পদ থেকে সরিয়ে দেওয়া হলো চৌধুরী আব্দুল্লাহ
+                      মামুনকে
                     </td>
                   </tr>
 
                   <tr>
                     <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 bg-[#EEF6FF] hover:text-blue-500">
-                    সংকট উত্তরণে দ্রুত চেইন অব কমান্ড প্রতিষ্ঠার চেষ্টায় পুলিশ
+                      সংকট উত্তরণে দ্রুত চেইন অব কমান্ড প্রতিষ্ঠার চেষ্টায় পুলিশ
                     </td>
-                    
                   </tr>
 
                   <tr>
                     <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 bg-[#EEF6FF] hover:text-blue-500">
-                    অজ্ঞাত স্থান থেকে আইজিপির ভিডিও বার্তা
+                      অজ্ঞাত স্থান থেকে আইজিপির ভিডিও বার্তা
                     </td>
                   </tr>
                 </tbody>
@@ -123,37 +126,92 @@ const LatestNewsDetails = () => {
 
             <h2 className="mb-5">{news_description.slice(151, 250)}</h2>
 
-            <hr className=" border-black opacity-80"/>
-              
-              <div className="flex justify-center mt-5">
-                  <img src={ksrm} alt="" />
+            <hr className=" border-black opacity-80" />
+
+            <div className="flex justify-center mt-5">
+              <img src={ksrm} alt="" />
+            </div>
+
+            <hr className="my-3  border-black opacity-80" />
+
+            <h2 className="p-3 ">{news_description.slice(251, 500)}</h2>
+
+            <div className="flex justify-center items-center my-5">
+              <Link
+                to={`https://www.whatsapp.com/channel/0029VaN81AfBvvscJ9n2xD2Y`}
+                target="_blank"
+                className="bg-[#249833] flex outline-none  justify-center w-max py-2.5 px-5 rounded text-white "
+              >
+                ঢাকাপোস্টচ্যানেল <MdWhatsapp className="mx-1"></MdWhatsapp> ফলো
+                করুন
+              </Link>
+            </div>
+
+            {/* 3 page */}
+            <div className="*:bg-[#E3E3E3] flex items-center gap-3 *:rounded-lg">
+              <Link className="p-3 lg:p-2 hover:text-cyan-900">ব্যাংক</Link>
+              <Link className="p-3 lg:p-2 hover:text-cyan-900">
+                বাংলাদেশ ব্যাংক
+              </Link>
+              <Link className="p-3 lg:p-2 hover:text-cyan-900">বিক্ষোভ</Link>
+            </div>
+
+            {/* adds */}
+
+            <div className="w-[250px] lg:w-[500px] mx-auto my-3">
+              <img className="w-full" src={adds} alt="" />
+            </div>
+            <hr className="border-gray-800" />
+
+            {/* sorboses */}
+            <div>
+              <h2 className="text-[#234E67] text-xl py-2">সর্বশেষ</h2>
+            </div>
+            <hr className="border-gray-800" />
+
+            {/* sorboses 3 card */}
+            <div className="flex flex-col lg:flex-row md:flex-row gap-5 ">
+
+              <div className="cursor-pointer overflow-hidden md:border-r-2 md:border-r-gray-800 lg:border-r-2 lg:border-r-gray-800 md:border-b-0 md:border-b-gray-50 border-b-2 lg:border-b-0 lg:border-b-gray-50 border-b-gray-800 pr-2 pt-2 hover:text-blue-700  ">
+                <figure  className="overflow-hidden">
+                  <img
+                    className="w-full lg:h-[165px] md:h-[170px] h-auto"
+                    src={`https://i.ibb.co/jG9HdWK/db-office-20240808002809.webp`}
+                    alt=""
+                  />
+                </figure>
+                <h2 className="my-3">
+                  ডিবিতে এখনও অনেকে আটক খবরে সেনাবাহিনীর অভিযান, ১৩ লাখ টাকা
+                  উদ্ধার
+                </h2>
               </div>
 
-              <hr className="my-3  border-black opacity-80"/>
-
-              <h2 className="p-3 ">{news_description.slice(251, 500)}</h2>
-
-              <div className="flex justify-center items-center my-5"> 
-                    <Link to={`https://www.whatsapp.com/channel/0029VaN81AfBvvscJ9n2xD2Y`} target="_blank" className="bg-[#249833] flex outline-none  justify-center w-max py-2.5 px-5 rounded text-white ">ঢাকাপোস্টচ্যানেল <MdWhatsapp className="mx-1"></MdWhatsapp> ফলো করুন</Link>
+              <div className="cursor-pointer overflow-hidden md:border-r-2 md:border-r-gray-800 lg:border-r-2 lg:border-r-gray-800 md:border-b-0 md:border-b-gray-50 border-b-2 lg:border-b-0 lg:border-b-gray-50 border-b-gray-800 pr-2 pt-2 hover:text-blue-700  ">
+                <figure className="overflow-hidden">
+                  <img
+                      className="w-full lg:h-[165px] md:h-[170px] h-auto"
+                    src={`https://i.ibb.co/Gn6Vwgt/satkhira-20240808000410.webp`}
+                    alt=""
+                  />
+                </figure>
+                <h2 className="my-3">
+                  হামলা-লুটপাট বন্ধে আইনশৃঙ্খলা বাহিনীকে সক্রিয় হওয়ার আহ্বান
+                </h2>
               </div>
 
-              {/* 3 page */}
-              <div className="*:bg-[#E3E3E3] flex gap-3 *:rounded-lg">
-                  <Link className="p-3 lg:p-2 hover:text-cyan-900">ব্যাংক</Link>
-                  <Link className="p-3 hover:text-cyan-900">বাংলাদেশ ব্যাংক</Link>
-                  <Link className="p-3 hover:text-cyan-900">বিক্ষোভ</Link>
+              <div className="cursor-pointer overflow-hidden  border-b-2 lg:border-b-0 lg:border-b-gray-50 border-b-gray-800 pr-2 pt-2  hover:text-blue-700 md:border-b-0 md:border-b-gray-50 ">
+                <figure  className="overflow-hidden">
+                  <img
+                     className="w-full lg:h-[165px] md:h-[170px] h-auto"
+                    src={`https://i.ibb.co/VBhz78L/hospital-20240807233138.webp`}
+                    alt=""
+                  />
+                </figure>
+                <h2 className="my-3">
+                  চিকিৎসা ব্যবস্থা ভেঙে পড়ছে, সেবা অব্যাহত রাখার নির্দেশনা
+                </h2>
               </div>
-
-              {/* adds */}
-
-              <div className="w-[250px] lg:w-[500px] mx-auto my-3">
-                 <img className="w-full" src={adds} alt="" />
-              </div>
-
-
-
-
-
+            </div>
           </div>
         </div>
 
@@ -163,10 +221,34 @@ const LatestNewsDetails = () => {
           </div>
           <hr />
 
-
           <h2>ঢাকা পোস্ট ভিডিও</h2>
           <div className="w-auto">
-            <img className="w-auto md:w-[50%] lg:w-full" src={jomunaAC} alt="" />
+            <img
+              className="w-auto md:w-[50%] lg:w-full"
+              src={jomunaAC}
+              alt=""
+            />
+          </div>
+          <div className="w-auto">
+            <img
+              className="w-auto md:w-[50%] lg:w-full"
+              src={`https://i.ibb.co/KFMJNZz/9349293546709784176.png`}
+              alt=""
+            />
+          </div>
+          <div className="w-auto">
+            <img
+              className="w-auto md:w-[50%] lg:w-full"
+              src={`https://i.ibb.co/72LB1PC/Model.png`}
+              alt=""
+            />
+          </div>
+          <div className="w-auto">
+            <img
+              className="w-auto md:w-[50%] lg:w-full"
+              src={`https://i.ibb.co/g6FhJxY/BG-1.jpg`}
+              alt=""
+            />
           </div>
         </div>
       </div>
